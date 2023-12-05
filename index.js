3de968cfaaf3d292e24d9947934098580f75fc7f
Kairi Wright <kanwright19@gmail.com>
2023-12-05 14:35:44 -0500

    Defining operators, Using increment & decrement, Elements of Parsing used.

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a,b) {
+ let sum = a + b; {
+    return sum;
+    }
+}
+
+function subtract(a,b) {
+ let difference = a - b; {
+    return difference;
+    }
+}
+
+function multiply (a,b) {
+ let product = a * b; {
+    return product;
+    }
+}
+
+function divide(a, b) {
+ let quotient = a / b; {
+    return quotient;
+    }
+}
+
+
+function increment(n) {
+    return n + 1;
+   }
+
+
+function decrement(n) {
+    return n - 1;
+}
+
+
+function makeInt(n) {
+    let base = 10
+ return parseInt(n,base);
+}
+
+
+function preserveDecimal(n) {
+    let base = 10
+ return parseFloat(n,base)
+}
